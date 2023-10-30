a794c21a79e820090f70638e3566c3911eb69cb2
FoxStorm1 <69159547+Yeetov@users.noreply.github.com>
2023-10-30 15:59:33 +0000

    Update index.tsx

--- userpfp/index.tsx
+++ userpfp/index.tsx
@@ -33,11 +33,16 @@ import "./userpfp.css";
 // Define the Vencord plugin
 export default definePlugin({
     name: "UserPFP",
-    authors: [{ name: "FoxStorm1", id: 789872551731527690n }, Devs.ImLvna, { name: "wolfieeeeeeee", id: 347096063569559553n }, { name: "dablulite", id: 582170007505731594n }],
+    authors: [
+        { name: "FoxStorm1", id: 789872551731527690n },
+        Devs.ImLvna,
+        { name: "wolfieeeeeeee", id: 347096063569559553n },
+        { name: "dablulite", id: 582170007505731594n }
+    ],
     description: "Custom animated profile pictures without Discord nitro.",
     settingsAboutComponent: () => {
         return (
-            
+            // Your settings about component content goes here
         );
     }
 });
