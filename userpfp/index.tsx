fb18957ad697dc5387e8ce6c61ba7ce75743128a
thororen <78185467+thororen1234@users.noreply.github.com>
2023-11-01 11:51:21 -0400

    tidy up

--- userpfp/index.tsx
+++ userpfp/index.tsx
@@ -14,38 +14,38 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+*/
 
-// Import additional modules and components
-import { openNotificationLogModal } from "@api/Notifications/notificationLog";
-import { Settings } from "@api/Settings";
-import ErrorBoundary from "@components/ErrorBoundary";
-import { Devs } from "@utils/constants";
-import { LazyComponent } from "@utils/react";
+// Import required modules and components
+import { enableStyle } from "@api/Styles";
+import { Link } from "@components/Link";
 import definePlugin from "@utils/types";
-import { findByCode } from "@webpack";
-import { Menu, Popout, useState } from "@webpack/common";
-import type { ReactNode } from "react";
 
-// Import the CSS file
-import "./userpfp.css";
+// Importing the style managed fixes on and off switch
+import style from "./userpfp.css?managed";
 
 // Define the Vencord plugin
 export default definePlugin({
     name: "UserPFP",
     authors: [
-        { name: "Yeetov", id: 789872551731527690n },
-        Devs.ImLvna,
-        { name: "wolfieeeeeeee", id: 347096063569559553n },
-        { name: "dablulite", id: 582170007505731594n }
+    { name: "Yeetov", id: 789872551731527690n },
+    { name: "Luna <3", id: 799319081723232267n },
+    { name: "wolfieeeeeeee", id: 347096063569559553n },
+    { name: "dablulite", id: 582170007505731594n },
+    { name: "thororen", id: 848339671629299742n }
     ],
     description: "Custom animated profile pictures without Discord nitro.",
     settingsAboutComponent: () => {
         return (
             <div>
                 <h1>About UserPFP</h1>
+                <Link href="https://discord.gg/userpfp-1129784704267210844">CLICK HERE FOR YOUR PFP</Link>
                 <p>Custom animated profile pictures without Discord nitro.</p>
             </div>
         );
+    },
+
+    async start() {
+        enableStyle(style);
     }
 });
